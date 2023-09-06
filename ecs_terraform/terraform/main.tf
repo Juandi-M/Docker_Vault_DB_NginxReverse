7b536729f2388a3d2524ecb19e5d56059577d669
Juandi-M <juanmonge95@gmail.com>
2023-09-06 13:58:34 -0600

    fixed main file

--- ecs_terraform/terraform/main.tf
+++ ecs_terraform/terraform/main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "terraform-poc-us-east-1"
+    profile = "default"
+    encrypt = true
+    key     = "vault-nginx-project/state_main"
+    region  = "us-east-1"
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
+}
+
+locals {
+  availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
 }
